7518962949ecea9fb1ff989c4e2f4da3776d495b
asomkyriakos <159484676+asomkyriakos@users.noreply.github.com>
2025-12-24 12:34:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,114 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstring>
+#include <unordered_map>
+#include <functional>
 using namespace std;
 
+void printHelp(){
+    cout << "Self-Driving Car Simulation" << endl;
+    cout << "Usage:" << endl;
+    cout << "  --seed <n>                   Random seed (default: current time)" << endl;
+    cout << "  --dimX <n>                   World width (default: 40)" << endl;
+    cout << "  --dimY <n>                   World height (default: 40)" << endl;
+    cout << "  --numMovingCars <n>          Number of moving cars (default: 3)" << endl;
+    cout << "  --numMovingBikes <n>         Number of moving bikes (default: 4)" << endl;
+    cout << "  --numParkedCars <n>          Number of parked cars (default: 5)" << endl;
+    cout << "  --numStopSigns <n>           Number of stop signs (default: 2)" << endl;
+    cout << "  --numTrafficLights <n>       Number of traffic lights (default: 2)" << endl;
+    cout << "  --simulationTicks <n>        Maximum simulation ticks (default: 100)" << endl;
+    cout << "  --minConfidenceThreshold <n> Minimum confidence cutoff (default: 40)" << endl;
+    cout << "  --gps <x1> <y1> [x2 y2 ...]  GPS target coordinates (required)" << endl;
+    cout << "  --help                       Show this help message" << endl << endl;
 
-int main(int argc, char* argv[]) {
+    cout << "Example usage:" << endl;
+    cout << "  ./oopp_proj_2025 --seed 12 --dimY 50 --gps 10 20 32 15" << endl;
+}
 
+//helper functions to parse cmd arguments
+bool isFlag(const char* s){
+    return strncmp(s, "--", 2) == 0;
+}
+
+bool parseIntOption(int& i, int argc, char* argv[], int& target){
+    if (i + 1 >= argc) {
+        return false;
+    }
+
+    target = stoi(argv[++i]);
+    return true;
+}
+
+bool parseGPS(int& i, int argc, char* argv[],vector<pair<int,int>>& gpsPoints){
+    ++i;
+    vector<int> values;
+
+    while (i < argc && !isFlag(argv[i])) {
+        values.push_back(stoi(argv[i]));
+        ++i;
+    }
+
+    --i; 
+
+    if (values.size() % 2 != 0) {
+        return false;
+    }
+
+    for (size_t j = 0; j < values.size(); j += 2) {
+        gpsPoints.emplace_back(values[j], values[j + 1]);
+    }
+
+    return true;
+}
+
+
+int main(int argc, char* argv[])
+{
+    int seed;
+    int dimX = 40;
+    int dimY = 40;
+    int numMovingCars = 3;
+    int numMovingBikes = 4;
+    int numParkedCars = 5;
+    int numStopSigns = 2;
+    int numTrafficLights = 2;
+    int simulationTicks = 100;
+    int minConfidenceThreshold = 40;
+
+    vector<pair<int,int>> gpsPoints;
+
+    for (int i = 1; i < argc; ++i) {
+        string arg = argv[i];
+
+        if (arg == "--help") {
+            printHelp();
+            return 0;
+        }
+        else if (arg == "--seed") {
+            if (!parseIntOption(i, argc, argv, seed)) return 1;
+        }
+        else if (arg == "--dimX") {
+            if (!parseIntOption(i, argc, argv, dimX)) return 1;
+        }
+        else if (arg == "--dimY") {
+            if (!parseIntOption(i, argc, argv, dimY)) return 1;
+        }
+        else if (arg == "--gps") {
+            if (!parseGPS(i, argc, argv, gpsPoints)) return 1;
+        }
+        else {
+            cerr << "Unknown option: " << arg << endl;
+            return 1;
+        }
+    }
+
+    if (gpsPoints.empty()) {
+        cout << "Error: --gps is required" << endl;
+        return 1;
+    }
+
+    //we start from here ,ok?
+
+    return 0;
 }
