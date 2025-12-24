d67ef84a67bc81ad829e50b6e9326d164025dbeb
asomkyriakos <159484676+asomkyriakos@users.noreply.github.com>
2025-12-24 14:41:11 +0200

    Update GridWorld.cpp

--- GridWorld.cpp
+++ GridWorld.cpp
@@ -1,47 +1,84 @@
+#include "GridWorld.h"
 #include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
 
-//Inheritable shit
-class MovingObjects{
-    private:
-        string id;
-        string glyph; //used in last question(question 4) don't bother yet
-};
-
-class StaticObject{
-    private: 
-        string id;
-        string glyph; //used in last question(question 4) don't bother yet
-
-};
-
-//Stationary classes with inheritance(rich daddy)
-
-class StationaryVehicles : public StaticObjects{
-    private:
-
-};
-
-class TrafficSigns : public StaticObjects{
-    private:
+// MovingObjects
+MovingObjects::MovingObjects(string i, string g, int xc, int xy): id(i), glyph(g), x(xc), y(xy) {
     
-};
+    }
+MovingObjects::~MovingObjects() {
 
-class TrafficLights : public StaticObjects{
-    private:
-        string colour; //use either R,Y,G to keep it simple
-        int tick; //make a method that changes the colour if tick goes from 0->4(red->green),4->12(green->orange),12->14(orange->red) on start we set the tick to the border number of the colour, will see how to do it
-    
-};
+}
+int MovingObjects::getX() const { 
+    return x; 
+}
+int MovingObjects::getY() const { 
+    return y; 
+}
+string MovingObjects::getGlyph() const { 
+    return glyph;
+}
+string MovingObjects::getId() const {
+     return id; 
+}
 
-//Moving classes with inheritance(they get yatch)
+// StaticObject
+StaticObject::StaticObject(string i, string g, int xc, int xy): id(i), glyph(g), x(xc), y(xy) {
 
-class Vehicle : public MovingObjects{
-    private:
-        int speed; //0 for stop, 1 for half speed, 2 for full speed
-        string direction; // NSEW system
-        string type; //either car or bike (C,B)
+}
+StaticObject::~StaticObject() {
 
-};
+}
+int StaticObject::getX() const { 
+    return x; 
+}
+int StaticObject::getY() const { 
+    return y; 
+}
+string StaticObject::getGlyph() const { 
+    return glyph; 
+}
+string StaticObject::getId() const { 
+    return id; 
+}
+
+// StationaryVehicles
+StationaryVehicles::StationaryVehicles(string id, string glyph, int x, int y, char tp): StaticObject(id, glyph, x, y), type(tp) {
+    cout << "Parked Made\n";
+}
+StationaryVehicles::~StationaryVehicles() { 
+    cout << "Parked Deleted\n"; 
+}
+
+// TrafficSigns
+TrafficSigns::TrafficSigns(string id, string glyph, int x, int y, char tp): StaticObject(id, glyph, x, y), type(tp) {
+    cout << "Signs Made\n";
+}
+TrafficSigns::~TrafficSigns() { 
+    cout << "Signs Deleted\n"; 
+}
+
+// TrafficLights
+TrafficLights::TrafficLights(string id, string glyph, int x, int y, char col, int tck): StaticObject(id, glyph, x, y), colour(col), tick(tck) {
+    cout << "Lights Made\n";
+}
+TrafficLights::~TrafficLights() { 
+    cout << "Lights Deleted\n"; 
+}
+
+// Vehicle
+Vehicle::Vehicle(string id, string glyph, int x, int y, int sp, char dir, char t): MovingObjects(id, glyph, x, y), speed(sp), direction(dir), type(t) {
+    cout << "Vehicle Made\n";
+}
+Vehicle::~Vehicle() { 
+    cout << "Vehicle Deleted\n"; 
+}
+int Vehicle::getSpeed() const { 
+    return speed; 
+}
+char Vehicle::getDirection() const { 
+    return direction; 
+}
+char Vehicle::getType() const { 
+    return type; 
+}
