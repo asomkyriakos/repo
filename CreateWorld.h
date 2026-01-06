f01646e1d1583b3a10b63b9d7579174831269a77
asomkyriakos <159484676+asomkyriakos@users.noreply.github.com>
2026-01-06 19:55:39 +0200

    Update CreateWorld.h

--- CreateWorld.h
+++ CreateWorld.h
@@ -1,12 +1,16 @@
 #pragma once
 #include <vector>
 #include "GridWorld.h" 
+#include "SelfDrivingCar.h"
 using namespace std;
+
+class Car;
 
 // a cell to keep entities in , helps with entities overlapping
 struct Cell {
     std::vector<MovingObjects*> movingObjects;
     std::vector<StaticObject*> staticObjects;
+    std::vector<Car*> car;
 };
 
 using Grid = std::vector<std::vector<Cell>>;
