d6250e8792626ae8c0f9b7a2c2721be76eb5998f
asomkyriakos <159484676+asomkyriakos@users.noreply.github.com>
2026-01-11 11:41:04 +0200

    Update DecisionMaking.h

--- DecisionMaking.h
+++ DecisionMaking.h
@@ -3,9 +3,9 @@
 #include <unordered_map>
 #include <string>
 #include <utility>
-#include "FuseSensorData.h" 
+#include "FuseSensorData.h"
 
 
 int manhattan(int x1, int y1, int x2, int y2);
 
-char decideNextMove(int carX, int carY, const Fuse& fused, const std::vector<std::pair<int,int>>& gpsPoints);
+std::pair<char, char> decideNextMove(int carX,int carY,char currentDir,int currentSpeed,const Fuse& fused,const std::vector<std::pair<int,int>>& gpsPoints);
